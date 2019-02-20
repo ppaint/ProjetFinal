0ffe5334f77fb1d4fbd951c51714eab73a5e37d1
vacca <clement.vacca1@outlook.com>
2019-02-20 09:49:32 +0100

    zardqz

---
+++ src/main/java/com/example/demo/restController/StagiaireRestController.java
@@ -0,0 +1,33 @@
+package com.example.demo.restController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Stagiaire;
+import com.example.demo.entity.jsonview.JsonViews;
+import com.example.demo.repository.StagiaireRepository;
+import com.fasterxml.jackson.annotation.JsonView;
+
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping("/rest/stagiaire")
+public class StagiaireRestController {
+
+	@Autowired
+	private StagiaireRepository stagiaireRepository;
+	
+	@JsonView(JsonViews.Common.class)
+	@GetMapping(value = { "", "/" })
+	public ResponseEntity<List<Stagiaire>> findAllGestionnaire() {
+		return new ResponseEntity<List<Stagiaire>>(stagiaireRepository.findAll(), HttpStatus.OK);
+	}
+	
+	
+}
